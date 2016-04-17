586521872d1cd6881ed5927018ac35f33b8b5bd0
Smasty <martin@smasty.net>
2016-04-17 03:32:49 +0200

    Implement Require task

--- lunaci/tasks/require.lua
+++ lunaci/tasks/require.lua
@@ -1,30 +1,23 @@
 module("lunaci.tasks.require", package.seeall)
 
-math.randomseed(os.time())
 
-
--- Dummy random placeholder task implementation.
-local require_modules = function(package, target, manifest)
+local require_modules = function(package, target, deploy_dir, manifest)
+    local utils = require "lunaci.utils"
     local config = require "lunaci.config"
-    err_msg = ([[
-Trying to require modules from package %s...
-Error: Something wrong happend.
-This is just a placeholder text here to make it longer.
-]]):format(tostring(package))
+    local tablex = require "pl.tablex"
+    local stringio = require "pl.stringio"
 
-    succ_msg = ([[
-Trying to require modules from package %s...
-All modules required successfully.
+    local modules = package.spec.build and package.spec.build.modules or {}
 
-Require test successful.
-]]):format(tostring(package))
-
-    rand = math.random(10)
-    if rand < 4 then
-        return config.STATUS_FAIL, err_msg, false
-    else
-        return config.STATUS_OK, succ_msg, true
+    local output = stringio.create()
+    local fail = false
+    for mod in tablex.sort(modules) do
+        local ok = utils.dir_exec(deploy_dir, "bin/lua -e 'require \"" .. mod .. "\"'")
+        output:writef("  %s\t %s\n", (ok and "OK" or "FAIL"), mod)
+        if not ok then fail = true end
     end
+
+    return fail and config.STATUS_FAIL or config.STATUS_OK, output:value(), not fail
 
 end
 
