bb2c9be1bdc9408278b68fbd760c558acc1881af
Smasty <martin@smasty.net>
2016-04-26 19:18:23 +0200

    Better implementation of Build task

--- lunaci/tasks/build.lua
+++ lunaci/tasks/build.lua
@@ -1,19 +1,36 @@
+-- LunaCI build task
+-- Part of the LuaDist project - http://luadist.org
+-- Author: Martin Srank, hello@smasty.net
+-- License: MIT
+
 module("lunaci.tasks.build", package.seeall)
 
 
--- Basic implementation. Needs better output handling.
 local build_package = function(package, target, deploy_dir, manifest)
     local config = require "lunaci.config"
     local utils = require "lunaci.utils"
 
     local ok, code, out, err = utils.dir_exec(deploy_dir, "bin/lua lib/lua/luadist.lua install '" .. package .. "'")
 
+    local msg = "Output:\n" .. out .. "\n"
     if not ok then
-        local msg = ("Exit code: %d\nSTDOUT:\n%s\n\nSTDERR:\n%s\n"):format(code, out, err)
+        msg = msg .. ("\nError:\n%s\nExit code: %d\n"):format(err, code)
+    end
+
+    if code == 1 then
+        return config.STATUS_INT, "Manifest retrieval failed.\n" .. msg, false
+    elseif code == 3 then
+        return config.STATUS_INT, "Package download failed.\n" .. msg, false
+    elseif code == 4 then
+        return config.STATUS_FAIL, "Installation of requested package failed.\n" .. msg, false
+    elseif code == 5 then
+        return config.STATUS_DEP, "Installation of a dependency failed.\n" .. msg, false
+    else
         return config.STATUS_FAIL, msg, false
     end
 
-    return config.STATUS_OK, out, true
+
+    return config.STATUS_OK, msg, true
 end
 
 
